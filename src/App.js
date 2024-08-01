ac21f4c00f8b8eafda52d56f1717c52985d237c6
Black Sheep <barbarblacksheep03@gmail.com>
2024-08-01 07:48:27 -0700

    initial application of mbgl

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
+import React, { useRef, useEffect, useState} from 'react';
+import mapboxgl from 'mapbox-gl';
 import './App.css';
-import HelloWorld from './HelloWorld';
-import mapboxgl from '!mapbox-gl';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker'; // Load worker code separately with worker-loader
+//import ReactSlider from 'react-slider';
+//import Select from 'react-select';
 
-mapboxgl.workerClass = MapboxWorker; // Wire up loaded worker to be used instead of the default
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9 // starting zoom
-});
+import { states } from './states.js';
 
-function App() {
+console.log(process.env);
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmxhY2tzaGVlcHNoYXduIiwiYSI6ImNseXlraGN5bDJiNTkycXE3am1keHhmOXIifQ.FZSVAtASwzAIKFfdLRVAPg';
+
+export default function App() {
+  //defaults
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  map.current.on('move', () => {
+    setLng(map.current.getCenter().lng.toFixed(4));
+    setLat(map.current.getCenter().lat.toFixed(4));
+    setZoom(map.current.getZoom().lng.toFixed(2));
+  });
+
+  let geoJsonForm = { 'type': 'FeatureCollection', 'features':[]};
+
+  console.log(states)
+  states.foeEach(elemetn => {
+    console.log(element);
+    let tempData = {
+      'type': 'Feature',
+      'properties': {
+        'name': element['name']
+      },
+      'geometry': {
+        'type': 'Polygon',
+        'coordinates': element['geometry']
+      }
+    }
+
+  });
+
+  //render the map
   return (
-    <div className="App">
-      <HelloWorld />
+    <div>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
 
-
-export default App;
